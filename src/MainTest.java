d0ec8cfb85b9a4f50d8edd3178ac1f0629723c07
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2019-09-17 14:11:48 +0200

    MainTest.java made to test program

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,10 +1,19 @@
 import junit.framework.TestCase;
 
 public class MainTest extends TestCase {
+    Calculator calc = new Calculator();
 
     public void testMain() {
-
-        System.out.println("+");
+        double a = calc.sum(4,5);
+        double b = calc.sub(5,4);
+        double c = calc.times(5,5);
+        double d = calc.divide(10,2);
+        double e = calc.divide(3,3);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
 
     }
 }
